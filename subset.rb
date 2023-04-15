4d7c2d21b13a75bf570ab1b4d04074cc4b29f32a
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-04-15 01:59:50 -0500

    add new solution iteration

--- subset.rb
+++ subset.rb
@@ -5,12 +5,25 @@
 
 # O(n^2) complexity 
 # 2x size?
+# def subset(input)
+#   result = []
+#   input.each_with_index do |i1, idx| # data structure 1
+#     input.drop(idx+1).each do |i2| # Possibly data structure 2
+#       result << [i1, i2]
+#     end
+#   end
+#   result
+# end
+
 def subset(input)
-  result = []
-  input.each_with_index do |i1, idx| # data structure 1
-    input.drop(idx+1).each do |i2| # Possibly data structure 2
-      result << [i1, i2]
+  combos = []
+  until input.size <= 1
+    first, *rest = input
+    rest.each do |i|
+      print rest
+      combos << [first, i]
     end
+    input = rest
   end
-  result
+  combos
 end
